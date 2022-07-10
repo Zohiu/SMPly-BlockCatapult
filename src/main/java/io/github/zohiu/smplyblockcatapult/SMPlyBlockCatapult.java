6f3b29d143f1ea228218fecda7c51e20f6fd2733
Zohiu <zohiu3@gmail.com>
2022-07-11 01:38:17 +0200

    Initial Commit

---
+++ src/main/java/io/github/zohiu/smplyblockcatapult/SMPlyBlockCatapult.java
@@ -0,0 +1,17 @@
+package io.github.zohiu.smplyblockcatapult;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SMPlyBlockCatapult extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
